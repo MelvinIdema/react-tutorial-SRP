04d444606efc64f8d1ab7d4f56cf6385f1bef323
Melvin Idema <melvinidema@melvins-mbp.home>
2021-04-30 13:49:28 +0200

    Finished Project IV

---
+++ project-iv/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {render} from "react-dom";
+
+import "./index.css";
+
+function App() {
+    return <p>Hello World!</p>;
+}
+
+render(<App />, document.querySelector("#react-root"));
