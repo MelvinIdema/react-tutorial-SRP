9fcabcb0b90bccb862863dc52879513a5602812b
Melvin Idema <melvinidema@melvins-mbp.home>
2021-05-10 17:34:58 +0200

    Change products api

--- 18-final-project/src/compositions/Products/index.js
+++ 18-final-project/src/compositions/Products/index.js
@@ -11,7 +11,7 @@ export default function Products(props) {
     );
 
     useEffect(() => {
-        get("supermarket.json")
+        get("bakery.json")
             .then((data) => {
                 setProducts(data);
             })
