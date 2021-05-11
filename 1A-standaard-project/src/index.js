f64314b115902735c41d26a55448ba5d0460783c
Melvin Idema <melvinidema@melvins-mbp.home>
2021-05-11 08:55:32 +0200

    Change root directory to standard project

--- 1A-standaard-project/src/index.js
+++ 1A-standaard-project/src/index.js
@@ -4,7 +4,15 @@ import {render} from "react-dom";
 import "./index.css";
 
 function App() {
-    return <p>Hello World!</p>;
+    return (<>
+        <h1>Development Website</h1>
+        <p>Welkom op mijn website!</p>
+        <p>Zoals elke Developer is ook mijn persoonlijke website niet af. Dit is ook meer een development environment. Voor een representatieve website kun je beter naar: <a href="http://www.melvinidema.nl/">melvinidema.nl</a> gaan.</p>
+        <h2>Projecten:</h2>
+        <ul>
+            <li><a href="https://react.ikbenmel.vin/">React Tutorial SRP</a></li>
+        </ul>
+    </>);
 }
 
 render(<App />, document.querySelector("#react-root"));
