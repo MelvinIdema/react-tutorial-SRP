59f3b91d9dbf078bf04adcdd05eda63e9550454e
Melvin Idema <melvinidema@melvins-mbp.home>
2021-05-11 08:49:47 +0200

    Change to deploy to netlify

--- 18-final-project/src/App.js
+++ 18-final-project/src/App.js
@@ -26,36 +26,17 @@ function App() {
   }, [cart]);
 
   function handleProductDelete(id) {
-    const updatedCart = cart.filter((product) => product.id !== id);
-    setCart(updatedCart);
+    setCart(cart.filter(item => item.id !== id));
   }
 
   function handleProductAdd(newProduct) {
-    // check if item exists
-    const existingProduct = cart.find(
-      (product) => product.id === newProduct.id
-    );
-    if (existingProduct) {
-      // increase quantity
-      const updatedCart = cart.map((product) => {
-        if (product.id === newProduct.id) {
-          return {
-            ...product,
-            quantity: product.quantity + 1,
-          };
-        }
-        return product;
+    const itemInCart = cart.find(item => item.id === newProduct.id);
+    if (itemInCart) {
+      const updatedCart = cart.map(item => {
+        if (item.id === newProduct.id) return {...item, quantity: item.quantity + 1}
+        return item;
       });
-      setCart(updatedCart);
-    } else {
-      // product is new to the cart
-      setCart([
-        ...cart,
-        {
-          ...newProduct,
-          quantity: 1,
-        },
-      ]);
+      return setCart(updatedCart);
     }
   }
 
